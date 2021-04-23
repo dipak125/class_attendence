f9d2e752e4464eac0f33f4f9d552a82aa6509a89
Rajiv Pandit <rajivp332@gmail.com>
2021-04-24 03:54:08 +0530

    Footer updated

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© Copyright 2021 Yodafy. All rights reserved.</p> 
+        </div>
+    )
+}
+export default Footer
