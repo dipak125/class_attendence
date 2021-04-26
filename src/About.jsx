408567178b6cedc6fc9896f97e8b239c33b274d9
Your Name <you@example.com>
2021-04-26 09:31:06 +0530

    Final About

--- src/About.jsx
+++ src/About.jsx
@@ -14,6 +14,7 @@ const About = () => {
             </span>
             <div className="image-data">
               <h3>Mayukh goswami </h3>
+              <p>Co-Founder of an Ed.Tech - Yodafy </p>
               <p class="lead"> Yodafy is a Education Fundamentals finishing school, where we help the college graduates become employable through Gamified training & help the companies meet their employee requirement through project directed training by Industry veterans.
               Yodafy has been instrumental in helping multiple client companies bridge their skill and employment gap by providing graduates training on Project specific and Extra Skillset in record time and least expense.
                 In case you want to know how Yodafy can help you grow or even discuss about ideas on how Yodafy can help meet your client and industry demand do E-mail at: mayukh.goswami@yodafy.in </p>
